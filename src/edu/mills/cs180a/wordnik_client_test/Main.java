491ad44f48e5f85ea73ce122ab712f4ed321bc63
Hannele <hjane@mills.edu>
2022-04-18 21:40:30 -0700

    Try to use API with limited success

--- src/edu/mills/cs180a/wordnik_client_test/Main.java
+++ src/edu/mills/cs180a/wordnik_client_test/Main.java
@@ -1,11 +1,11 @@
 package edu.mills.cs180a.wordnik_client_test;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-import edu.mills.cs180a.wordnik.client.api.WordsApi;
-import edu.mills.cs180a.wordnik.client.invoker.ApiClient;
-import edu.mills.cs180a.wordnik.client.model.WordOfTheDay;
+import java.io.*;
+import java.nio.charset.*;
+import java.util.*;
+import edu.mills.cs180a.wordnik.client.api.*;
+import edu.mills.cs180a.wordnik.client.invoker.*;
+import edu.mills.cs180a.wordnik.client.model.*;
 
 public class Main {
     private static String getApiKey() throws IOException {
@@ -13,8 +13,7 @@ public class Main {
     }
 
     private static String getResource(String filename) throws IOException {
-        try (InputStream is =
-                Main.class.getResourceAsStream(filename)) {
+        try (InputStream is = Main.class.getResourceAsStream(filename)) {
             if (is == null) {
                 throw new IOException("Unable to open file " + filename);
             }
@@ -25,7 +24,12 @@ public class Main {
     public static void main(String[] args) throws IOException {
         ApiClient client = new ApiClient("api_key", getApiKey());
         WordsApi wordsApi = client.buildClient(WordsApi.class);
+        WordApi wordApi = client.buildClient(WordApi.class);
         WordOfTheDay word = wordsApi.getWordOfTheDay("2022-03-15");
-        System.out.println(word);
+        List<WordObject> randomWords =
+                wordsApi.getRandomWords("true", "", "", null, -1, 1, -1, 5, -1, "", "", 10);
+        WordObject random =
+                wordsApi.getRandomWord(null, null, null, null, null, null, null, null, null);
+        System.out.println(wordApi.getScrabbleScore("none"));
     }
 }
